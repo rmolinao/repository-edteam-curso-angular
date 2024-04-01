9635899a79f91c2777ce3312217a12b0cff1e7e3
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-04-01 13:38:19 -0500

    observaciones testeando la aplicacion con ng test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,18 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  /*------------------------- !!! 01-ABR-22024(RJM) -------------------------------+
+  | beforeEach(...); se define bloque de codigo que se ejecuta antes de cada test |
+  +-------------------------------------------------------------------------------*/
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
   });
 
+  /*--------------- !!! 01-ABR-22024(RJM) -------------+
+  | las instrucciones 'it(...);' son casos de pruebas |
+  +---------------------------------------------------*/
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
@@ -24,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, edteam-curso-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Bienbenido a edteam-curso-angular');
   });
 });
