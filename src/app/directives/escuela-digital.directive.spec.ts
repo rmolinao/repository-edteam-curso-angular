f2b729035cf37a76857e7b02324b03b942235fa9
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-04-09 06:57:39 -0500

    generando una directiva  con angular CLI - ng generate directive ...

---
+++ src/app/directives/escuela-digital.directive.spec.ts
@@ -0,0 +1,8 @@
+import { EscuelaDigitalDirective } from './escuela-digital.directive';
+
+describe('EscuelaDigitalDirective', () => {
+  it('should create an instance', () => {
+    const directive = new EscuelaDigitalDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/escuela-digital.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appEscuelaDigital]',
+  standalone: true
+})
+export class EscuelaDigitalDirective {
+
+  constructor() { }
+
+}
