07e410909dbe76090ec24961a121325e99ac081f
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-04-09 06:10:45 -0500

    generando un componentes con angular CLI - ng generate componet ...

---
+++ src/app/components/escuela-digital/escuela-digital.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-escuela-digital',
+  standalone: true,
+  imports: [],
+  templateUrl: './escuela-digital.component.html',
+  styleUrl: './escuela-digital.component.css'
+})
+export class EscuelaDigitalComponent {
+
+}
