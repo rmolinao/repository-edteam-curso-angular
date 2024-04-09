80da7245e949e939035d4e15bf714a67ae9b9876
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-04-09 06:59:07 -0500

    generando un pipe  con angular CLI - ng generate pipe ...

---
+++ src/app/pipes/filtro.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filtro',
+  standalone: true
+})
+export class FiltroPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
