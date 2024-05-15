c26ee49c82fafc3b481e5f69c0dc650470bf44f6
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-05-14 22:34:52 -0500

    agrego el nuevo componenete a la plantilla del  componenete principal e importo el nuevo componenete al componenete principal

--- projects/mini-proyecto/src/app/app.component.ts
+++ projects/mini-proyecto/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EscuelaDigitalComponent } from '../../escuela-digital/escuela-digital.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EscuelaDigitalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
