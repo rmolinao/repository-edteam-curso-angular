e17657114c63e9015e87634cd53632e686216438
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-06-08 15:57:06 -0500

    se limpia plantilla principal y se muestran los componenetes creados

--- projects/mini-proyecto/src/app/app.component.ts
+++ projects/mini-proyecto/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EscuelaDigitalComponent } from '../../escuela-digital/escuela-digital.component';
+import { NavbarComponent } from '../../navbar/navbar.component';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { SidenavComponent } from '../../sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EscuelaDigitalComponent],
+  imports: [
+    RouterOutlet,
+    EscuelaDigitalComponent,
+    NavbarComponent,
+    SidenavComponent,
+    DashboardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
