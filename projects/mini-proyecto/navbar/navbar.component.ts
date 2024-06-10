2ba3766f59e982e6796a09f9c31a31db6b18ec42
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-06-10 09:07:28 -0500

    se aplican estilos del  componente navbar en el proyecto

--- projects/mini-proyecto/navbar/navbar.component.ts
+++ projects/mini-proyecto/navbar/navbar.component.ts
@@ -5,12 +5,38 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      navbar works!
-    </p>
+    <section>
+      @if (isloggedIn) {
+        <p>
+          <img
+            (click)="greet()"
+            src="https://github.com/rmolinao.png"
+            alt="photo"
+          >
+        </p>
+      }@else {
+        <p>Inicia sesion</p>
+        <button (click)="isloggedIn=true">Log In</button>
+      }
+    </section>
   `,
-  styles: ``
+  styles: `
+    section {
+      max-width: 500px;
+      margin: 0 auto;
+      padding-top: 32px;
+      font-size: 24px;
+    }
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+  `
 })
 export class NavbarComponent {
-
+  isloggedIn = false;
+  greet (){
+    alert('!!! Hola');
+  };
 }
