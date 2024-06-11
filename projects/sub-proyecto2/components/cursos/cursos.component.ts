b07895130551d6863b34f1f64736ab0c6fa2e7af
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-06-10 19:34:42 -0500

    creo componente cursos en sub-proyecto2 aplico el concepto de interpolacion en la pantilla del componente

---
+++ projects/sub-proyecto2/components/cursos/cursos.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="card">
+      <div class="card-header">
+        {{titulo}}
+      </div>
+      <div class="card-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Logo</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Desripcion</th>
+              <th scope="col">Fecha Inicio</th>
+              <th scope="col">Preico</th>
+              <th scope="col">Rating</th>
+              <th scope="col">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            @for (curso of cursos; track curso.id) {
+              <tr>
+                <th scope="row">{{curso.id}}</th>
+                <td>{{curso.name}}</td>
+                <td>{{curso.descripttion}}</td>
+                <td>{{curso.startDate}}</td>
+                <td>{{curso.price}}</td>
+                <td>{{curso.rating}}</td>
+                <td>{{curso.img}}</td>
+              </tr>
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  `,
+  styles: ``
+})
+export class CursosComponent {
+  titulo :string ='Lista de Cursos';
+  cursos:any = [
+    { id:1,
+      name:'TypeSrcipt Desde Cero',
+      startDate:'August 10, 2009',
+      descripttion:'lleva javaScript al siguiente Nivel ...',
+      price:25.99,
+      rating:4.5,
+      img:'assets/images/typescript.png'
+    },
+    { id:2,
+      name:'Angular Desde Cero',
+      startDate:'September 10, 2009',
+      descripttion:'Aprende el framework frondend ...',
+      price:25.99,
+      rating:4.5,
+      img:'assets/images/typescript.png'
+    },
+  ];
+}
