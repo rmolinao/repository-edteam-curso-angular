53390a8eeafd564ec29d3c91ad45d1b8e996a5dc
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-06-26 21:19:07 -0500

    creo un fichero  cursos.ts en el cual voy a definir el listado de cursos amostrar

---
+++ projects/sub-proyecto2/src/app/data/courses.ts
@@ -0,0 +1,20 @@
+import { Curso } from "../../../interfaces/curso";
+
+export const cursos:Curso[]  = [
+        { id:1,
+        name:'TypeSrcipt Desde Cero',
+        startDate:'August 10, 2009',
+        descripttion:'lleva javaScript al siguiente Nivel ...',
+        price:25.99,
+        rating:4.5,
+        img:'assets/images/typescript.png'
+        },
+        { id:2,
+        name:'Angular Desde Cero',
+        startDate:'September 10, 2009',
+        descripttion:'Aprende el framework frondend ...',
+        price:25.99,
+        rating:4.5,
+        img:'assets/images/angular.png'
+        },
+    ];
