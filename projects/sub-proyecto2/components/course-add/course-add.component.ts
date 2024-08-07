addd8d619e0c2f2c7b217e34f299fc94ee659e13
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-08-07 11:41:47 -0500

    creo  el componenete course-add en sub-projecto2

---
+++ projects/sub-proyecto2/components/course-add/course-add.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-add',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      course-add works!
+    </p>
+  `,
+  styles: ``
+})
+export class CourseAddComponent {
+
+}
