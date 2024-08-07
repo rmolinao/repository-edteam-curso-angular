d6fc5a9f0b3ba3d4a4344ca2eaa6f31ac0376786
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-08-07 15:29:22 -0500

    se configura formulario crear curso

--- projects/sub-proyecto2/components/course-add/course-add.component.ts
+++ projects/sub-proyecto2/components/course-add/course-add.component.ts
@@ -5,9 +5,38 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      course-add works!
-    </p>
+<div class="container-md ">
+  <div class="row justify-content-center align-items-center">
+    <div class="col-6">
+      <div class="card mt-4" >
+        <div class="card-header">Nuevo Curso</div>
+        <div class="card-body">
+          <form>
+
+            <div class=" form-group mb-3">
+              <label for="name" class="form-label">Nombre</label>
+              <input type="text" class="form-control" id="name" aria-describedby="emailHelp">
+            </div>
+            <div class=" form-group mb-3">
+              <label for="price" class="form-label">Precio</label>
+              <input type="number" class="form-control" id="price" aria-describedby="emailHelp">
+            </div>
+            <div class=" form-group mb-3">
+              <label for="imageUrl" class="form-label">Url de imagen</label>
+              <input type="text" class="form-control" id="imageUrl" aria-describedby="emailHelp">
+            </div>
+
+            <div class="form-group form-check mb-3">
+              <input type="checkbox" class="form-check-input" id="active">
+              <label for="active" class="form-check-label">Curso Activo</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Crear Curso</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   `,
   styles: ``
 })
