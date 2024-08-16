f9e24d2fc69d14ebf211e40f324368f8020628fc
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-08-16 07:17:18 -0500

    se configura input nombre como requerido con cu configuracion de ngModel para que pueda detectar el estado valido e invalido

--- projects/sub-proyecto2/components/course-add/course-add.component.ts
+++ projects/sub-proyecto2/components/course-add/course-add.component.ts
@@ -6,47 +6,73 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   template: `
-<div class="container-md ">
-  <div class="row justify-content-center align-items-center">
-    <div class="col-6">
-      <div class="card mt-4" >
-        <div class="card-header">Nuevo Curso</div>
-        <div class="card-body">
-          <form #fomrAdd="ngForm">
+    <div class="container-md ">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-6">
+          <div class="card mt-4">
+            <div class="card-header">Nuevo Curso</div>
+            <div class="card-body">
+              <form #fomrAdd="ngForm">
+                <div class=" form-group mb-3">
+                  <label for="name" class="form-label">Nombre</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    aria-describedby="emailHelp"
+                    name="name"
+                    [(ngModel)]="nombre"
+                    #name="ngModel"
+                    required
+                  />
+                </div>
+                <div class=" form-group mb-3">
+                  <label for="price" class="form-label">Precio</label>
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="price"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div class=" form-group mb-3">
+                  <label for="imageUrl" class="form-label">Url de imagen</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="imageUrl"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
 
-            <div class=" form-group mb-3">
-              <label for="name" class="form-label">Nombre</label>
-              <input type="text" class="form-control" id="name" aria-describedby="emailHelp">
-            </div>
-            <div class=" form-group mb-3">
-              <label for="price" class="form-label">Precio</label>
-              <input type="number" class="form-control" id="price" aria-describedby="emailHelp">
-            </div>
-            <div class=" form-group mb-3">
-              <label for="imageUrl" class="form-label">Url de imagen</label>
-              <input type="text" class="form-control" id="imageUrl" aria-describedby="emailHelp">
-            </div>
-
-            <div class="form-group form-check mb-3">
-              <input type="checkbox" class="form-check-input" id="active">
-              <label for="active" class="form-check-label">Curso Activo</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Crear Curso</button>
-          </form>
-          <div>
-            <pre>
-              Form.valid:{{fomrAdd.form.valid}}
-              Form.invalid:{{fomrAdd.form.invalid}}
+                <div class="form-group form-check mb-3">
+                  <input type="checkbox" class="form-check-input" id="active" />
+                  <label for="active" class="form-check-label"
+                    >Curso Activo</label
+                  >
+                </div>
+                <button type="submit" class="btn btn-primary">
+                  Crear Curso
+                </button>
+              </form>
+              <div>
+                <pre>
+              Form.valid:{{ fomrAdd.form.valid }}
+              Form.invalid:{{ fomrAdd.form.invalid }}
             </pre>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
   `,
-  styles: ``
+  styles: ``,
 })
 export class CourseAddComponent {
+  nombre: string ='';
+  constructor(){
+
+  }
 
 }
